e5adf0f676adbdbd09e14eba4a92d28e260305b3
mylesglass <mylesalexanderglass@gmail.com>
2026-07-26 17:07:56 +1200

    highlight Gymnasts nav on /gymnast/... routes

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -131,7 +131,7 @@
           </a>
           <a
             href="/gymnasts"
-            class="btn btn-sm {active('/gymnasts') ? 'btn-primary' : 'btn-ghost'}"
+            class="btn btn-sm {active('/gymnasts') || currentPath.startsWith('/gymnast/') ? 'btn-primary' : 'btn-ghost'}"
           >
             Gymnasts
           </a>
@@ -270,7 +270,7 @@
         </a>
       </li>
       <li>
-        <a href="/gymnasts" onclick={handleNavClick} class:active={active("/gymnasts")}>
+        <a href="/gymnasts" onclick={handleNavClick} class:active={active("/gymnasts") || currentPath.startsWith("/gymnast/")}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="size-4 fill-current"><path d="M10 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM3.465 14.493a1.23 1.23 0 0 0 .41 1.412A9.957 9.957 0 0 0 10 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 0 0-13.074.003Z" /></svg>
           Gymnasts
         </a>
