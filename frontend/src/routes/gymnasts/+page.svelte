ed19163e3c6ca6c8d164ccb435d9679c6edbfd33
mylesglass <mylesalexanderglass@gmail.com>
2026-07-26 17:58:56 +1200

    center letter nav

--- frontend/src/routes/gymnasts/+page.svelte
+++ frontend/src/routes/gymnasts/+page.svelte
@@ -79,7 +79,7 @@
     />
   </div>
 
-  <div class="flex flex-wrap gap-1 mb-4">
+  <div class="flex flex-wrap gap-1 mb-4 justify-center">
     {#each Object.keys(grouped) as letter}
       <button
         class="btn btn-xs btn-outline"
